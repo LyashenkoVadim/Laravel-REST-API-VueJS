717e07e97d3a06128c5f8e6fff8090549ad8418a
Tera <30482211+Worstoftheworst@users.noreply.github.com>
2019-08-05 10:11:17 +1000

    create simple filter for books

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -21,45 +21,11 @@ class BookController extends Controller
     public function index(Request $request)
     {
 
-        // {
-        //     "per_page": "2",
-        //     "book_name": "Non at illo.",
-        //     "book_num_pages": "540",
-        //     "library_id": "30",
-        //     "created_at": "2019-07-01 03:17:41",
-        //     "updated_at": "2019-07-01 03:17:41"
-        // }
-        if(count($request->query())){
-
-            if($request->per_page){
-
-            }
-
-            return count($request->query());
-        }
-
-        $books = Book::paginate(4);
+        $books = Book::where($request->except('per_page'))->paginate(4);
         return BookResource::collection($books);
-        // $books = Book
-        //                 ::where('book_name', "Non at illo.")
-        //                 ->where('book_num_pages', 540)
-        //                 ->where('library_id', 35)
-        //                 ->where('created_at', "2019-07-01 03:17:41")
-        //                 ->where('updated_at', "2019-07-01 03:17:41")
-        //                 ->get();
 
-        // if($request->book_name){
-        //     $books = Book::where('library_id', $request->book_name)->get();
-        // }
-        // if($request->library_id){
-        //     $books = Book::where('library_id', $request->library_id)->get();
-        // }
-        // if($request->book_num_pages){
-        //     $books = Book::where('book_num_pages', $request->book_num_pages)->get();
-        // }
-        return count($request->query());
-        $per_page = ApiService::PerPageHandler($request);
-        return ApiService::PaginationHandler($per_page, 4, $class=Book::class, $class=BookResource::class);
+        // $per_page = ApiService::PerPageHandler($request);
+        // return ApiService::PaginationHandler($per_page, 4, $class=Book::class, $class=BookResource::class);
     }
 
     public function store(BookRequest $request)
